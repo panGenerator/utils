6271bdfddef335e91fc39fa60c8c7c81d6cfc480
Krzysztof Goliński <krzysztof@golinski.org>
2020-05-18 14:18:34 +0200

    preparing for publish

--- utils.js
+++ utils.js
@@ -81,6 +81,7 @@ function lerpColor (a, b, amount) {
   var ah = parseInt(a.replace(/#/g, ''), 16); var ar = ah >> 16; var ag = ah >> 8 & 0xff; var ab = ah & 0xff; var bh = parseInt(b.replace(/#/g, ''), 16); var br = bh >> 16; var bg = bh >> 8 & 0xff; var bb = bh & 0xff; var rr = ar + amount * (br - ar); var rg = ag + amount * (bg - ag); var rb = ab + amount * (bb - ab)
   return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1)
 }
+
 function precision (value, precision) {
   return Math.round(value * Math.pow(10, precision)) / Math.pow(10, precision)
 }
