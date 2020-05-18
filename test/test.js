e7c361a25cdc516f8b897cbeb8c810973cdcf259
Krzysztof Goliński <krzysztof@golinski.org>
2020-05-18 17:03:38 +0200

    UMD, eslint

---
+++ test/test.js
@@ -0,0 +1,31 @@
+// import { map, clamp, random, randomDir, lerp, lerp3, lerpedPoints, dist, square, norm, degrees, radians, randomName, timestampName, randomIndex, shuffleArray, lerpColor, precision } from '../utils.js'
+const { map, clamp, random } = require('../utils')
+const assert = require('assert')
+
+describe('Utils', function () {
+  describe('#map()', function () {
+    it('should map the value from source range to provided range', function () {
+      assert.equal(map(0.5, 0, 1, 0, 100), 50)
+      assert.equal(map(0.1, 0, 1, 0, 100), 10)
+    })
+  })
+
+  describe('#clamp()', function () {
+    it('should clamp the value to provided range', function () {
+      assert.equal(clamp(0, 1, 10), 1)
+      assert.equal(clamp(100, 1, 10), 10)
+      assert.equal(clamp(5, 1, 10), 5)
+    })
+  })
+
+  describe('#random()', function () {
+    it('should generate random number from provided range', function () {
+      const min = 5
+      const max = 10
+      let r = random(min, max)
+      assert(r >= min && r < max)
+      r = random(max)
+      assert(r >= 0 && r < max)
+    })
+  })
+})
--- utils.js
+++ utils.js
@@ -1,89 +1,103 @@
-function map (value, low1, high1, low2, high2) {
-  return low2 + (high2 - low2) * (value - low1) / (high1 - low1)
-}
-
-function clamp (val, min, max) {
-  return val > max ? max : val < min ? min : val
-}
-
-function random (low, high) {
-  if (high === undefined) {
-    high = low
-    low = 0
+/* global define */
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory)
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory()
+  } else {
+    root.returnExports = factory()
   }
-  return low + Math.random() * (high - low)
-}
+}(typeof self !== 'undefined' ? self : this, function () {
+  /* ----- START ----- */
 
-function randomDir () {
-  return Math.random() > 0.5 ? 1 : -1
-}
-
-function lerp (start, stop, amt) {
-  return start + (stop - start) * amt
-}
-
-function lerp3 (A, B, amt) {
-  return { x: lerp(A.x, B.x, amt), y: lerp(A.y, B.y, amt), z: lerp(A.z, B.z, amt) }
-}
-
-function lerpedPoints (A, B, count) {
-  const points = []
-  const step = 1 / (count + 1)
-  for (let i = 0; i < count; i++) {
-    points.push(lerp3(A, B, step + step * i))
+  function map (value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1)
   }
-  return points
-}
 
-function dist (A, B) {
-  return Math.sqrt(square(B.x - A.x) + square(B.y - A.y) + square(B.z - A.z))
-}
-
-function square (a) {
-  return a * a
-}
-
-function norm (value, start, stop) {
-  return (value - start) / (stop - start)
-}
-
-function degrees (radians) {
-  return radians * 180.0 / Math.PI
-}
-
-function radians (degrees) {
-  return degrees * Math.PI / 180.0
-}
-
-function randomName (N) {
-  return (Math.random().toString(36) + '00000000000000000').slice(2, N + 2)
-}
-
-function timestampName () {
-  var tzoffset = (new Date()).getTimezoneOffset() * 60000
-  let date = (new Date(Date.now() - tzoffset)).toISOString().replace(/z|t/gi, ' ').trim().replace(/:/gi, '-')
-  date = date.substring(0, date.indexOf('.'))
-  return date
-}
-
-function randomIndex (N) {
-  return Math.floor(Math.random() * N)
-}
-
-function shuffleArray (array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]
+  function clamp (val, min, max) {
+    return val > max ? max : val < min ? min : val
   }
-}
 
-function lerpColor (a, b, amount) {
-  var ah = parseInt(a.replace(/#/g, ''), 16); var ar = ah >> 16; var ag = ah >> 8 & 0xff; var ab = ah & 0xff; var bh = parseInt(b.replace(/#/g, ''), 16); var br = bh >> 16; var bg = bh >> 8 & 0xff; var bb = bh & 0xff; var rr = ar + amount * (br - ar); var rg = ag + amount * (bg - ag); var rb = ab + amount * (bb - ab)
-  return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1)
-}
+  function random (low, high) {
+    if (high === undefined) {
+      high = low
+      low = 0
+    }
+    return low + Math.random() * (high - low)
+  }
 
-function precision (value, precision) {
-  return Math.round(value * Math.pow(10, precision)) / Math.pow(10, precision)
-}
+  function randomDir () {
+    return Math.random() > 0.5 ? 1 : -1
+  }
 
-export { map, clamp, random, randomDir, lerp, lerp3, lerpedPoints, dist, square, norm, degrees, radians, randomName, timestampName, randomIndex, shuffleArray, lerpColor, precision }
+  function lerp (start, stop, amt) {
+    return start + (stop - start) * amt
+  }
+
+  function lerp3 (A, B, amt) {
+    return { x: lerp(A.x, B.x, amt), y: lerp(A.y, B.y, amt), z: lerp(A.z, B.z, amt) }
+  }
+
+  function lerpedPoints (A, B, count) {
+    const points = []
+    const step = 1 / (count + 1)
+    for (let i = 0; i < count; i++) {
+      points.push(lerp3(A, B, step + step * i))
+    }
+    return points
+  }
+
+  function dist (A, B) {
+    return Math.sqrt(square(B.x - A.x) + square(B.y - A.y) + square(B.z - A.z))
+  }
+
+  function square (a) {
+    return a * a
+  }
+
+  function norm (value, start, stop) {
+    return (value - start) / (stop - start)
+  }
+
+  function degrees (radians) {
+    return radians * 180.0 / Math.PI
+  }
+
+  function radians (degrees) {
+    return degrees * Math.PI / 180.0
+  }
+
+  function randomName (N) {
+    return (Math.random().toString(36) + '00000000000000000').slice(2, N + 2)
+  }
+
+  function timestampName () {
+    var tzoffset = (new Date()).getTimezoneOffset() * 60000
+    let date = (new Date(Date.now() - tzoffset)).toISOString().replace(/z|t/gi, ' ').trim().replace(/:/gi, '-')
+    date = date.substring(0, date.indexOf('.'))
+    return date
+  }
+
+  function randomIndex (N) {
+    return Math.floor(Math.random() * N)
+  }
+
+  function shuffleArray (array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+  }
+
+  function lerpColor (a, b, amount) {
+    var ah = parseInt(a.replace(/#/g, ''), 16); var ar = ah >> 16; var ag = ah >> 8 & 0xff; var ab = ah & 0xff; var bh = parseInt(b.replace(/#/g, ''), 16); var br = bh >> 16; var bg = bh >> 8 & 0xff; var bb = bh & 0xff; var rr = ar + amount * (br - ar); var rg = ag + amount * (bg - ag); var rb = ab + amount * (bb - ab)
+    return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1)
+  }
+
+  function precision (value, precision) {
+    return Math.round(value * Math.pow(10, precision)) / Math.pow(10, precision)
+  }
+  return { map, clamp, random, randomDir, lerp, lerp3, lerpedPoints, dist, square, norm, degrees, radians, randomName, timestampName, randomIndex, shuffleArray, lerpColor, precision }
+
+  /* ----- END ----- */
+}))
